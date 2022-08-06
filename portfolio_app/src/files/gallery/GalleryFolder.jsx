bbb224b496428c9f5d487e0307a58a36c298c9c5
Ryan Kustaborder <rkusty23@gmail.com>
2022-08-06 10:59:15 -0400

    checkpoint

--- portfolio_app/src/files/gallery/GalleryFolder.jsx
+++ portfolio_app/src/files/gallery/GalleryFolder.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 
 import _rug from "./rug.jpg";
+import _sketch from "../../sketches/sketch";
 
 import FolderFileIcon from "../../k95/fileIcons/FolderFileIcon";
 import PhotoFileIcon from "../../k95/fileIcons/PhotoFileIcon";
+import P5FileIcon from "../../k95/fileIcons/P5FileIcon";
 
 export default class GalleryFolder extends Component {
   render() {
@@ -22,6 +24,16 @@ export default class GalleryFolder extends Component {
           useDarkText={true}
           image={_rug}
         ></PhotoFileIcon>
+        <P5FileIcon
+          onAddWindow={this.props.onAddWindow}
+          onRemoveWindow={this.props.onRemoveWindow}
+          onSelectWindow={this.props.onSelectWindow}
+          title={"sketch.p5"}
+          useDarkText={true}
+          sketch={_sketch}
+          initState={{ saturation: 100 }}
+          getInputs={() => {}}
+        ></P5FileIcon>
       </FolderFileIcon>
     );
   }
